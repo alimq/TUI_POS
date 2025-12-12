930bd0a256962e660c5be7021e17bd7faad9715e
alimq <alimimanmalik2@gmail.com>
2025-12-12 13:54:22 +0800

    first commit

---
+++ classes.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+from sqlalchemy import Column, Integer, String,\
+    Numeric, DateTime, func
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    price = Column(Numeric(10,2))
+    stock_quantity = Column(Integer, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now())
+
+class Order(Base):
+    __tablename__ = 'order'
+    id = Column(Integer, primary_key=True)
+    total = Column(Numeric(10,2), nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+
+from sqlalchemy import ForeignKey
+
+class OrderItem(Base):
+    __tablename__ = 'order_item'
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey('order.id'), nullable=False)
+    product_id = Column(Integer, ForeignKey('product.id'), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    subtotal = Column(Numeric(10,2), nullable=False)
+
+from sqlalchemy import CheckConstraint
+
+class Payment(Base):
+    __tablename__ = 'payment'
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey('order.id'))
+    amount = Column(Numeric(10,2), nullable=False)
+    method = Column(String(7),
+                    CheckConstraint("method IN ('ewalllet','cash')"
+                                    ,name="method"),
+                    nullable=False
+                    )
+    created_at = Column(DateTime, server_default=func.now())
---
+++ main.py
@@ -0,0 +1,32 @@
+from classes import *
+engine = create_engine("sqlite:///our.db")
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import Session
+session = Session(bind=engine)
+# session.add(), session.commit(), session.query()
+
+def hash(p):
+    return p
+
+h = {
+    'cashier': 'hash123',
+    'admin': 'hash123',
+}
+
+def load(u):
+    print(u, 'loaded!')
+
+while True:
+    user = input("Input the user: ")
+    if user == 'cashier' or user == 'admin':
+        while True:
+            password = input("Input the password: ")
+            if hash(password) == h[user]:
+                load(user)
+                break
+            else:
+                print("Incorrect password. Try again")
+        break
+    else:
+        print("User does not exist. Try again")
